acdd7528c9f83bf56c4b6c2c1ae16620b89cf8b9
latino-deluxe <emanuelelatino2206@gmail.com>
2019-02-12 12:55:01 +0100

    implementazione parking bar, sensori IR teorici

--- src/parkingbar.cpp
+++ src/parkingbar.cpp
@@ -1,3 +1,5 @@
+#include "vars.h"
+
 void checkButton() {
   V1 = digitalRead(PU1);
   if(V1 == HIGH) {
@@ -13,10 +15,35 @@ void checkIR() {
   VIR3 = analogRead(IR3);
   if(VIR1<500) entry2 = true;
   else entry2 = false;
+  if(VIR2<500) exit1 = true;
+  else exit1 = false;
+  if(VIR3<500) exit2 = true;
+  else exit2 = false;
 }
 
 void parkingBar1() {
   checkButton();
-  if(entry1) for(int i=0; i<=90; i++) mariottide.write(i);
+  checkIR();
+  if(entry1) for(int i=0; i<=90; i++) {
+    mariottide.write(i);
+    delay(5);
+  }
+  if(entry2) mariottide.write(90);
+  if(entry2 == false) for(int i=90; i>=0; i--) {
+    mariottide.write(i);
+    delay(5);
+  }
+}
 
+void parkingBar2() {
+  checkIR();
+  if(exit1) for(int i=0; i<=90; i++) {
+    mariottide.write(i);
+    delay(5);
+  }
+  if(exit2) mariottide.write(90);
+  if(exit2 == false) for(int i=90; i>=0; i--) {
+    mariottide.write(i);
+    delay(5);
+  }
 }
